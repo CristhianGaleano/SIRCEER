07a9ccca6dc2d46a7c1705044d24fa91b9ff4867
Cristhian Alexis Galeano Ruiz <titiruizah@gmail.com>
2020-01-07 22:58:21 -0500

    Se integra satisfactoriamente las nuevas librerias

--- view/cabecera.php
+++ view/cabecera.php
@@ -3,6 +3,18 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Sistema de registro y control estudiantes de risaralda</title>
+
+	<!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <!-- CSS personalizado --> 
+    <!---<link rel="stylesheet" href="main.css">  -->
+      
+      
+    <!--datables CSS básico-->
+    <link rel="stylesheet" type="text/css" href="../datatables/datatables.min.css"/>
+    <!--datables estilo bootstrap 4 CSS-->  
+    <link rel="stylesheet"  type="text/css" href="../datatables/DataTables-1.10.18/css/dataTables.bootstrap4.min.css">
+
 	<link rel="stylesheet" href="../css/estilos.css">
 </head>
 <body>
--- view/piedepagina-admin.php
+++ view/piedepagina-admin.php
@@ -1,4 +1,15 @@
 </div>
+
+
+<!-- jQuery, Popper.js, Bootstrap JS -->
+    <script src="bootstrap/js/jquery-3.3.1.min.js"></script>
+    <script src="bootstrap/js/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+      
+    <!-- datatables JS -->
+    <script type="text/javascript" src="datatables/datatables.min.js"></script>    
+     
+    <script type="text/javascript" src="main.js"></script>  
 </body>
 	<footer>
 	
