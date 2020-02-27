b5f4d1eee14d9a2242cb467dc3e5eb0492d14b80
Cristhian Alexis Galeano Ruiz <titiruizah@gmail.com>
2020-02-26 22:25:41 -0500

    Historial aca

--- gestion/consultar-historia-aca.php
+++ gestion/consultar-historia-aca.php
@@ -12,7 +12,14 @@ comprobarConexion($cn);
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
 	#var_dump($_POST);
-    
+	  $doc=cleanData($_POST['documento']);
+	 # var_dump($doc);
+	  $estudiante = getAllStudentRelations($doc,$cn);
+// var_dump($estudiante);
+$matricula = getMatricula($estudiante['id'],$cn);
+// var_dump($matricula);
+$historial = getHistorialEstudiante(doc,$cn);
+	  #var_dump($estudiante);
 	
 }
 
--- view/consultar-historia.view.php
+++ view/consultar-historia.view.php
@@ -19,11 +19,11 @@
  
       <div class="row main_wraper">
         <div class="col-12">
-          <form action="<?php echo URL  ?>gestion/historia.php" method="GET" >
+          <form action="<?php echo htmlspecialchars( $_SERVER['PHP_SELF']) ?>" method="POST" >
               <div class="form-row align-items-center">
                 <div class="col-auto">
-                  <label class="sr-only" for="inlineFormInput">Name</label>
-                  <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Documento">
+                  <label class="sr-only" for="inlineFormInput">Documento</label>
+                  <input type="text" class="form-control mb-2" name="documento" id="inlineFormInput" placeholder="Documento">
                 </div>
 
                  <div class="col-auto">
